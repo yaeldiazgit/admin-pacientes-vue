d34d4494b6dcfce5a9d8c3d8165acc69404e1800
Yael Díaz <yaeldiaz.ygds@gmail.com>
2024-03-20 01:31:53 -0600

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+  import { ref, reactive } from "vue";
+  import Header from './components/Header.vue';
+  import Formulario from './components/Formulario.vue';
+
+  const paciente = reactive({
+        nombre: '',
+        propietario: '',
+        email: '',
+        alta: '', 
+        sintomas: ''
+    });
+
+  const pacientes = ref([]);
+</script>
+
+<template>
+    <div class="container mx-auto mt-20 ">
+      <Header/>
+
+      <div class="mt-12 md:flex">
+        <Formulario
+          v-model:nombre="paciente.nombre"
+          v-model:propietario="paciente.propietario"
+          v-model:email="paciente.email"
+          v-model:alta="paciente.alta"
+          v-model:sintomas="paciente.sintomas"  
+        />
+
+        <div class="md:w-1/2 md:h-screen overflow-y-scroll">
+          <h3 class="font-black text-3xl text-center">
+            Administrar tus Pacientes
+          </h3>
+
+          <div v-if="pacientes.length > 0">
+
+          </div>
+
+          <p v-else class="mt-20 text-2xl text-center">
+            No hay pacientes
+          </p>
+
+        </div>
+      </div>
+    </div>
+</template>
+
+
---
+++ src/components/Formulario.vue
@@ -0,0 +1,149 @@
+<script setup>
+    import { reactive } from "vue";
+    import Alerta from "./Alerta.vue";
+
+
+    const props = defineProps({
+        nombre: {
+            type: String,
+            required: true
+        },
+        propietario: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        alta: {
+            type: String,
+            required: true
+        },
+        sintomas: {
+            type: String,
+            required: true
+        },
+    });
+    
+    const alerta = reactive({
+        mensaje: '',
+        tipo: ''
+    })
+
+    const validar = () => {
+        if(Object.values(props).includes('')){
+            alerta.mensaje = 'Todos los campos son obligatorios';
+            alerta.tipo = 'error';
+            return
+        }
+        alerta.mensaje = 'Guardado correctamente...';
+        alerta.tipo = 'ok';
+    }
+
+    defineEmits(['update:nombre', 'update:propietario', 'update:email', 'update:alta', 'update:sintomas']);
+
+</script>
+
+<template>
+    <div class="md:w-1/2">
+        <h2 class="font-black text-3xl text-center ">Seguimiento Pacientes</h2>
+
+        <p class="text-lg mt-5 text-center mb-10">
+            Añade pacientes y <span class="text-indigo-600 font-bold">Administralos</span>
+        </p>
+        <Alerta
+            v-if="alerta.mensaje"
+            :alerta="alerta"
+        />
+        <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+            @submit.prevent="validar">
+            <div class="mb-5">
+                <label 
+                    for="mascota"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Nombre Mascota
+                </label>
+                <input 
+                    type="text" 
+                    id="mascota"
+                    placeholder="Nombre de la mascota"
+                    class="border-2 w-full p-2 placeholder-gray-400 rounded-md"
+                    :value="nombre"
+                    @input="$emit('update:nombre', $event.target.value)"
+                />
+
+
+                <label 
+                    for="propietario"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Nombre del Propietario
+                </label>
+                <input 
+                    type="text" 
+                    id="propietario"
+                    placeholder="Nombre del propietario"
+                    class="border-2 w-full p-2 placeholder-gray-400 rounded-md"
+                    :value="propietario"
+                    @input="$emit('update:propietario', $event.target.value)"
+                />
+
+
+                <label 
+                    for="email"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Email
+                </label>
+                <input 
+                    type="email" 
+                    id="email"
+                    placeholder="Email del propietario"
+                    class="border-2 w-full p-2 placeholder-gray-400 rounded-md"
+                    :value="email"
+                    @input="$emit('update:email', $event.target.value)"
+                />
+
+
+                <label 
+                    for="alta"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Alta
+                </label>
+                <input 
+                    type="date" 
+                    id="alta"
+                    class="border-2 w-full p-2 placeholder-gray-400 rounded-md"
+                    :value="alta"
+                    @input="$emit('update:alta', $event.target.value)"
+                />
+
+
+                <label 
+                    for="sintomas"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Sintomas
+                </label>
+                <textarea 
+                    id="sintomas"
+                    placeholder="Describe los sintomas"
+                    class="border-2 w-full p-2 placeholder-gray-400 rounded-md h-40"
+                    :value="sintomas"
+                    @input="$emit('update:sintomas', $event.target.value)"
+                />
+
+                <input 
+                    type="submit"
+                    class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 
+                    cursor-pointer transition-colors " 
+                    value="Registrar Paciente"/>
+            </div>
+
+        </form>
+
+    </div>
+</template>
